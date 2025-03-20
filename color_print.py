6276655b8d984704e61f28a88a7f4b9ad4f1bd28
Epitaph <168910038+EpitaphNewell@users.noreply.github.com>
2025-03-20 10:31:04 +0100

    Update color_print

--- color_print.py
+++ color_print.py
@@ -1,19 +1,30 @@
-import traceback
-
 def format_text(text, error=None):
     codes = {
         "{re}": "\033[91m",
         "{yl}": "\033[93m",
         "{gr}": "\033[92m",
         "{bl}": "\033[94m",
+        "{pu}": "\033[95m",
         "{rs}": "\033[0m"
     }
 
     if "{er}" in text:
-        err_text = "[ERROR]"
+        err_text = "[ОШИБКА]"
         if error:
             err_text += f" {error}"
-        text = text.replace("{er}", f"\033[91m{err_text}\033[0m")
+        text = text.replace("{er}", f"{codes.get("{re}")}{err_text}{codes.get("{rs}")}")
+
+    if "{wr}" in text:
+        err_text = "[ПРЕДУПРЕЖДЕНИЕ]"
+        if error:
+            err_text += f" {error}"
+        text = text.replace("{wr}", f"{codes.get("{yl}")}{err_text}{codes.get("{rs}")}")
+
+    if "{if}" in text:
+        err_text = "[ИНФО]"
+        if error:
+            err_text += f" {error}"
+        text = text.replace("{if}", f"{codes.get("{bl}")}{err_text}{codes.get("{rs}")}")
 
     for key, value in codes.items():
         text = text.replace(key, value)
@@ -22,10 +33,3 @@ def format_text(text, error=None):
         text += "\033[0m"
 
     return text
-
-def printc(text, error=None):
-    print(format_text(text, error))
-
-def print_error(e):
-    error_msg = traceback.format_exc().strip().split("\n")[-1]
-    printc("{er}", error_msg)
